854e16191218eb36d2864e77fc90a10a40d334f9
Kevin Bongcasan <kbongcasmis@gmail.com>
2025-01-18 20:13:04 -0600

    Added api hookup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { genVillageService } from "./services/genVillageService";
 
 function App() {
+  const [healthStatus, setHealthStatus] = useState<string>("");
+
+  const checkHealth = async () => {
+    await genVillageService.healthCheck().then((response) => {
+      console.log(response.data);
+      if (response.status === 200) {
+        setHealthStatus(response.data as string);
+      } else {
+        setHealthStatus("Service is down. Please try again later.");
+      }
+    });
+  };
+
+  useEffect(() => {
+    checkHealth();
+  }, []);
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Kevin Provident cupiditate voluptatem et in. Quaerat fugiat ut
-            assumenda excepturi exercitationem quasi. In deleniti eaque aut
-            repudiandae et a id nisi.
-          </p>
+          {healthStatus === "" ? (
+            <p className="py-6">Not good</p>
+          ) : (
+            <p className="py-6">{healthStatus}</p>
+          )}
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
---
+++ src/services/genVillageService.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosResponse } from "axios";
+
+// Define the base URL
+const GENVILLAGE_SERVICE_URL = "/api/genvillage";
+const baseURL = GENVILLAGE_SERVICE_URL;
+
+// Define types for the responses and parameters
+interface HealthCheckResponse {
+  message: string;
+}
+
+interface GenVillageResponse {
+  id: string;
+}
+
+// Health check function
+const healthCheck = async (): Promise<AxiosResponse<HealthCheckResponse>> => {
+  return await axios.get<HealthCheckResponse>(`${baseURL}/healthCheck`);
+};
+
+// Generate village function
+const genVillage = async (
+  theme: string
+): Promise<AxiosResponse<GenVillageResponse>> => {
+  const data = new FormData();
+  data.append("theme", theme);
+  return (await axios.post)<GenVillageResponse>(baseURL, data);
+};
+
+// Export the service
+export const genVillageService = {
+  genVillage,
+  healthCheck,
+};
